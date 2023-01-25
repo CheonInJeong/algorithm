8dda62cf711bc582e36e41eba51ba23f9b29f8aa
chenchen <burub3@gmail.com>
2023-01-25 23:54:14 +0900

    Stack 구현

--- src/main/java/com/example/algorithm/dataStructure/Stack.java
+++ src/main/java/com/example/algorithm/dataStructure/Stack.java
@@ -1,2 +1,42 @@
-package com.example.algorithm.dataStructure;public class Stack {
+package com.example.algorithm.dataStructure;
+
+import java.util.NoSuchElementException;
+
+public class Stack<T> {
+    class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    private Node<T> top;
+
+    public T pop() {
+        if (top == null) {
+            throw new NoSuchElementException();
+        }
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    public T peek() {
+        if (top == null) {
+            throw new NoSuchElementException();
+        }
+        return top.data;
+    }
+
+    public void push(T data) {
+        Node<T> t = new Node<>(data);
+        t.next = top;
+        top = t;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
 }
--- src/test/java/com/example/algorithm/dataStructure/dataStruecture.java
+++ src/test/java/com/example/algorithm/dataStructure/dataStruecture.java
@@ -22,7 +22,23 @@ public class dataStruecture {
         Assertions.assertEquals(false, queue.isEmpty());
         Assertions.assertEquals(4, queue.remove());
         Assertions.assertEquals(true, queue.isEmpty());
-     
+    }
+
+    @Test
+    public void stack() {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        Assertions.assertEquals(4, s.pop());
+        Assertions.assertEquals(3, s.pop());
+        Assertions.assertEquals(2, s.peek());
+        Assertions.assertEquals(2, s.pop());
+        Assertions.assertEquals(false, s.isEmpty());
+        Assertions.assertEquals(1, s.pop());
+        Assertions.assertEquals(true, s.isEmpty());
     }
 
 }
